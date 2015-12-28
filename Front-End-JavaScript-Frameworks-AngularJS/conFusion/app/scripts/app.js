300ce1ba58375bfb497a669cc653dc0c5a19b852
Joshua Maagdy <josh.magady@gmail.com>
2015-12-27 18:21:08 -0600

    cofigured for use with routes

--- Front-End-JavaScript-Frameworks-AngularJS/conFusion/app/scripts/app.js
+++ Front-End-JavaScript-Frameworks-AngularJS/conFusion/app/scripts/app.js
@@ -1,6 +1,30 @@
 'use strict';
 
-angular.module('confusionApp', [])
+angular.module('confusionApp', ['ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider
+
+            // route for the home page
+            .when('/contactus', {
+                templateUrl : 'contactus.html',
+                controller : 'ContactController'
+            })
+
+            // route for the menu page
+            .when('/menu', {
+                templateUrl : 'menu.html',
+                controller : 'MenuController'
+            })
+
+            // route for the dish details page
+            .when('/menu/:id', {
+                templateUrl : 'dishdetail.html',
+                controller : 'DishDetailController'
+            })
+
+            // route for anything else
+            .otherwise('/contactus');
+    })
 
 ;
 
